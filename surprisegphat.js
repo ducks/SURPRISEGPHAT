88e04aa1ede6106a19750d6bac8f991dd5c464df
Jake Goldsborough <rjgoldsborough@gmail.com>
2015-05-21 10:00:04 -0400

    making x positioning random for more surprises

--- surprisegphat.js
+++ surprisegphat.js
@@ -10,9 +10,13 @@ var div = document.createElement('div');
 div.className = "surprise-gphat";
 div.style.backgroundImage = "url(https://www.runscope.com/static/img/public/customer-portrait-keenio.png)";
 div.style.position = "fixed";
-div.style.right = "15%";
+div.style.left = randomIntBtwn(1, 75) + '%';
 div.style.bottom = "0";
 div.style.height = "355px";
 div.style.width = "350px";
 
 document.getElementsByTagName('body')[0].appendChild(div);
+
+function randomIntBtwn(min, max) {
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
